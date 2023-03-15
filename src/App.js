b2de6988e37f09dc6b80c239fb100dc6c5fe2328
Kshitij Kumar <kshitij27kumar@gmail.com>
2023-03-15 21:46:40 +0530

    Cards component and Data added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import Cards from './components/Cards'
+import data from './components/Data'
 import Navbar from './components/Navbar.jsx'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className='flexed'>
+        {data.map((val, index) => {
+          return (
+            <Cards
+              key={index}
+              imgsrc={val.imgscr}
+              pname={val.pname}
+              product={val.product}
+              price={val.price}
+            />
+          )
+        })}
+      </div>
     </>
   )
 }
